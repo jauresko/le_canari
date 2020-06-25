c55fc3c8098eea79fd9f30621b1f6cc500db7748
jaures kouassi <jauresko@gmail.com>
2020-06-25 23:44:18 +0100

    email integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_many :reviews
   has_one_attached :photo
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :confirmable, :validatable
   validates :username , :first_name, :last_name, :phone_number, :email, presence: true
   validates :username, uniqueness: true
 
